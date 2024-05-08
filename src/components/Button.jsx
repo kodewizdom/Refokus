2372b9d7713eee18501af846e35b63fb163f36df
sajid <mdskhan4774@gmail.com>
2024-05-08 20:46:45 +0530

    Completed Product structure

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { IoIosReturnRight } from "react-icons/io";
 
-const Button = () => {
+const Button = ({title="Get Started"}) => {
   return (
-    <div className="min-w-20 px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center gap-4">
-        <span className="text-sm font-normal text-zinc-800">Get Started</span>
+    <div className="min-w-40 px-4 py-2 bg-zinc-100 text-black rounded-full inline-flex items-center justify-between">
+        <span className="text-sm font-normal text-zinc-800">{title}</span>
         <IoIosReturnRight />
     </div>
   )
