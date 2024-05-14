02416e729fa040f9e832cd06223ecd7dea04777b
sajid <mdskhan4774@gmail.com>
2024-05-14 19:22:02 +0530

    Completed Marquee component animation

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
 
-const Marquee = ({imagesurls}) => {
+const Marquee = ({ imagesurls, direction }) => {
   return (
-    <div className="flex items-center w-full py-8 gap-20 whitespace-nowrap overflow-hidden">
-        {imagesurls.map(url=><img src={url} className=""/>)}
+    <div className="flex w-full overflow-hidden">
+      <motion.div
+        initial={{ x: direction ==='left'? "0":"-100%"}}
+        animate={{ x: direction ==='left'? "-100%":"0" }}
+        transition={{repeat:Infinity,ease:"linear", duration:15}}
+        className="flex items-center w-full py-8 pr-40 gap-20 flex-shrink-0"
+      >
+        {imagesurls.map((url) => (
+          <img src={url} className="" />
+        ))}
+      </motion.div>
+      <motion.div
+        initial={{ x: direction ==='left'? "0":"-100%"}}
+        animate={{ x: direction ==='left'? "-100%":"0" }}
+        transition={{repeat:Infinity,ease:"linear", duration:15}}
+        className="flex items-center w-full py-8 pr-40 gap-20 flex-shrink-0"
+      >
+        {imagesurls.map((url) => (
+          <img src={url} className="" />
+        ))}
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Marquee
+export default Marquee;
